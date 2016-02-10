2f6749d9dc3d86ed93fa62b870f3c8446743cc27
Samuel <srousser@gmail.com>
2016-02-10 10:54:14 -0600

    Made HumanPlayer and edited Player class to include new variables

---
+++ src/player/HumanPlayer.java
@@ -0,0 +1,17 @@
+package player;
+import board.Grid;
+
+/**
+ * Created by Student on 2/10/2016.
+ */
+public class HumanPlayer extends Player{
+    Grid grid;
+
+    public void update(Grid grid) {
+        if (active == true) {
+            if (grid.clicked != null) {
+                move = grid.clicked.col;
+            }
+        }
+    }
+}
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,13 +1,26 @@
 package player;
 
-public class Player {
+import board.Grid;
+
+public abstract class Player {
 
 	public String name;
 	public int symbol;
+	public boolean active;
+	public boolean movemade;
+	public int move;
+
+	public Player() {
+		name = "Player1";
+		symbol = 1;
+		active = false;
+	}
 
 	public Player(String name, int symbol) {
 		this.name = name;
 		this.symbol = symbol;
+		active = false;
 	}
 
+	abstract void update(Grid grid);
 }
